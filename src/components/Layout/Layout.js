7511bfdd38733fc67b56d313d547885929f157a8
viktar hameza <viktor.hameza@gmail.com>
2019-04-23 20:41:16 +0300

    logIn

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,7 +4,23 @@ import './Layout.css';
 
 class Layout extends PureComponent {
   render() {
-    return 'empty';
+    const { children } = this.props;
+    return (
+      
+      <Fragment>
+        <header className="header">
+          <SectionTitle className="header__title">Header</SectionTitle>
+          <div className="header__content">тест</div>
+        </header>
+        <main className="main main--with-header main--with-footer">
+          <SectionTitle className="main__title">Main</SectionTitle>
+          {children}
+        </main>
+        <footer className="footer">
+          <SectionTitle className="footer__title">Footer</SectionTitle>
+        </footer>
+      </Fragment>
+    );
   }
 
   renderHeader(HeaderChild) {
--- src/contexts/Auth/Auth.js
+++ src/contexts/Auth/Auth.js
@@ -3,9 +3,38 @@ import React, { PureComponent } from 'react';
 const { Provider, Consumer: AuthConsumer } = React.createContext('');
 
 class AuthProvider extends PureComponent {
+  state = {
+    email: '',
+    authorizeError: '',
+    isAuthorized: false
+  };
+
+  authorize = (email, password) => {
+    if (email === 'stu@dent.com' && password === '123') {
+      this.setState({
+        email,
+        authorizeError: '',
+        isAuthorized: true
+      });
+    } else {
+      this.setState({ authorizeError: 'Email или пароль введён не верно' });
+    }
+  };
+
+  logout = () => this.setState({ isAuthorized: false });
+
+  getProviderValue = () => {
+    return {
+      ...this.state,
+      authorize: this.authorize,
+      logout: this.logout
+    };
+  };
+
   render() {
     const { children } = this.props;
-    return <Provider>{children}</Provider>;
+
+    return <Provider value={this.getProviderValue()}>{children}</Provider>;
   }
 }
 
